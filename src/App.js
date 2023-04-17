15b26c414f2ab02d783ffc6e6a9d81857ba3c4ae
jaume vidal <jaumevibu@gmail.com>
2023-04-17 09:04:21 +0200

    merge feature/footer into dev

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Footer from './components/Footer/Footer';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
+
   return (
     <Router>
-    <div className="App">
-        <Footer/>
-    </div>
+      <div className="App">
+        <Footer />
+      </div>
     </Router>
   );
 }
