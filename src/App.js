05f30cf0b2c98889ec8ae0708e4fd8452bb1f068
jaume vidal <jaumevibu@gmail.com>
2023-04-17 09:14:17 +0200

    merge feature/searchbar into dev

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import Footer from './components/Footer/Footer';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
+
   return (
     <Router>
       <div className="App">
