ede2e73f6d88d22719b313c2188ddbee0d1b5420
mihaelagheorghiu10 <118915363+mihaelagheorghiu10@users.noreply.github.com>
2023-04-18 15:14:16 +0200

    add components in Home

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import './App.css';
-import Footer from './components/Footer/Footer';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css'
+import Footer from './components/Footer/Footer'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
-
   return (
     <Router>
       <div className="App">
         <Footer />
       </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Pages/HomePage/HomePage.jsx
+++ src/components/Pages/HomePage/HomePage.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import styles from './homePage.module.css'
+//import Searchbar from '../../Searchbar/Searchbar'
 
 export default function HomePage() {
-  return <div>HomePage</div>
+  return (
+    <div className={styles.homeContainer}>
+      <div className={styles.searchContainer}>
+        <input className={styles.input} type="text"></input>
+        <button className={styles.button}>buscar</button>
+      </div>
+    </div>
+    //<Searchbar>
+  )
 }
