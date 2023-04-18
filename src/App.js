82e806941f45745d164b0154b489b537880ca002
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-04-18 15:05:14 +0200

    create login page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './pages/Register/Register'
+import Login from './pages/Login/Login'
 
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
       <div className="App">
         <Navbar/>
         <Routes>
-          <Route path="/register" element={<Register/>}></Route>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/login" element = {<Login/>}/>
         </Routes>
         <Footer />
       </div>
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import style from './login.module.css'
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
+
+export default function Login() {
+    let loginSchema = Yup.object().shape({
+        email: Yup.string()
+          .email("No es un email válido")
+          .required("Campo obligatorio"),
+        password: Yup.string()
+          .min(8, "El número mínimo de caracteres es 8")
+          .max(30, "El número maximo de caracteres es 30")
+          .required("Campo obligatorio"),
+      });
+
+      const formik = useFormik({
+        initialValues: {
+          email: "",
+          password: "",
+        },
+        validationSchema: loginSchema,
+        
+        onSubmit: (values) => {
+          console.log(values);
+        },
+      });
+
+  return (
+    <div className={style.loginPage}>
+      <form className={style.loginForm} onSubmit={formik.handleSubmit}>
+        
+        <div className={style.labelInput}>
+          <label for="email">Email</label>
+          <input
+            name="email"
+            id="email"
+            type="text"
+            placeholder="email@email.com"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+          />
+          {formik.errors.email ? (
+          <div className={style.errorToast}>{formik.errors.email}</div>
+        ) : null}
+        </div>
+        <div className={style.labelInput}>
+          <label for="password">Contraseña</label>
+          <input
+            name="password"
+            id="password"
+            type="password"
+            placeholder="contraseña"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+          />
+          {formik.errors.password ? (
+          <div className={style.errorToast}>{formik.errors.password}</div>
+        ) : null}
+        </div>
+
+        <button className={style.loginButton} type="submit">Login</button>
+      </form>
+    </div>
+  )
+}
