6b4b91e17a1613191f4b3fc173a47b58e093b79c
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-04-21 10:00:02 +0200

    add link to event list in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import styles from './navbar.module.css'
 import userImage from '../Assets/Image20230417113019.png'
+import {Link} from 'react-router-dom'
 
 export default function Navbar() {
   return (
     <div className={styles.divContenedor}>
       <div className={[styles.navbar, styles.navbarLeft].join(' ')}>
-        <h1 className={styles.h1Title}>Tech Events</h1>
+        <Link className={styles.link} to={"/events"}>
+          <h1 className={styles.h1Title}>Tech Events</h1>
+        </Link>
       </div>
       <div className={[styles.navbar, styles.navbarRight].join(' ')}>
         <p>Usuario</p>
