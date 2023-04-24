b6964d38e1266a8350bcb52831411e7921a0284a
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-04-24 14:33:19 +0200

    add preview card to form

--- src/pages/EventForm/EventForm.jsx
+++ src/pages/EventForm/EventForm.jsx
@@ -16,7 +16,8 @@ export default function EventForm() {
       title: "",
       date: "",
       time: "",
-      confirmPassword: "",
+      image: "",
+      description: "",
     },
     validationSchema: eventSchema,
     onSubmit: async (values) => {
@@ -27,6 +28,7 @@ export default function EventForm() {
     <div className={style.eventFormPage}>
       <div className={style.eventForm}>
         <h2>Editar Evento</h2>
+        <hr />
         <div className={style.labelInput}>
           <label htmlFor="title">Título</label>
           <input
@@ -63,17 +65,49 @@ export default function EventForm() {
             />
           </div>
         </div>
-        <div className={style.imageInput}>
-          <div className={style.labelInput}>
-            <label htmlFor="title">Título</label>
-            <input
-              name="title"
-              id="title"
-              type="text"
-              value={formik.values.title}
-              onChange={formik.handleChange}
+
+        <div className={style.labelInput}>
+          <label htmlFor="image">Imagen</label>
+          <input
+            name="image"
+            id="image"
+            type="file"
+            value={formik.values.image}
+            onChange={formik.handleChange}
+          />
+        </div>
+        <div className={style.labelInput}>
+          <label htmlFor="description">Descripción</label>
+          <textarea
+            name="description"
+            id="description"
+            type="text"
+            value={formik.values.description}
+            onChange={formik.handleChange}
+            required
+            rows={6}
+          />
+        </div>
+      </div>
+      <div className={style.previewForm}>
+        <h2>Previsualizar</h2>
+        <hr />
+        <div className={style.cardPreviewForm}>
+          <h2 className={style.eventTitle}>{"event.title"}</h2>
+          <div className={style.eventPhotoContainer}>
+            <img
+              className={style.eventPhoto}
+              src={"event.photo"}
+              alt={`Foto de ${"event.title"}`}
             />
           </div>
+          <p className={style.eventDate}>{`${"event.date"} ${"event.time"}`}</p>
+          <p className={style.eventMaxParticipants}>
+            <strong>Aforo: </strong>
+            {`${"event.max_participants"} participantes`}
+          </p>
+          <h4 className={style.eventDetailHeader}>Detalles: </h4>
+          <p className={style.eventDescription}>{"event.description"}</p>
         </div>
       </div>
     </div>
