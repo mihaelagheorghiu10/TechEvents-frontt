49ecb686ce625ad0082d99345423c1e5bf0e0517
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-04-26 12:32:22 +0200

    add conditionals to display name and image according to authed boolean

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import styles from './navbar.module.css'
-import userImage from '../Assets/Image20230417113019.png'
+import userImage from '../Assets/user.png'
 import {Link} from 'react-router-dom'
+import { useAuth } from '../../hooks/useAuth'
 
 export default function Navbar() {
+  const auth = useAuth();  
+  console.log(auth);
+  const toggleMenu = () => {
+    
+  }
   return (
     <div className={styles.divContenedor}>
       <div className={[styles.navbar, styles.navbarLeft].join(' ')}>
@@ -12,12 +18,15 @@ export default function Navbar() {
         </Link>
       </div>
       <div className={[styles.navbar, styles.navbarRight].join(' ')}>
-        <p>Usuario</p>
+        <p>{auth.authed? "usuario" : "Invitado"}</p> 
+        <div className={styles.imgContainer}>
         <img
-          src={userImage}
+          src={auth.authed? "usuario" : userImage}
           className={styles.userImage}
           alt="imagen Usuario"
+          onClick={()=>toggleMenu()}
         />
+        </div>
       </div>
     </div>
   )
